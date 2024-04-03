5a5613a04c8ec8c221c333cf1dfeceaa3f62910c
zuihou <244387066@qq.com>
2024-04-03 20:08:10 +0800

    refactor: 抽取变量

--- lamp-base/lamp-base-server/src/main/java/top/tangyh/lamp/base/config/BaseWebConfiguration.java
+++ lamp-base/lamp-base-server/src/main/java/top/tangyh/lamp/base/config/BaseWebConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import top.tangyh.basic.boot.config.BaseConfig;
+import top.tangyh.basic.constant.Constants;
 import top.tangyh.basic.log.event.SysLogListener;
 import top.tangyh.basic.utils.BeanPlusUtil;
 import top.tangyh.lamp.base.service.system.BaseOperationLogService;
@@ -22,7 +23,7 @@ public class BaseWebConfiguration extends BaseConfig {
      * lamp.log.enabled = true 并且 lamp.log.type=DB时实例该类
      */
     @Bean
-    @ConditionalOnExpression("${lamp.log.enabled:true} && 'DB'.equals('${lamp.log.type:LOGGER}')")
+    @ConditionalOnExpression("${" + Constants.PROJECT_PREFIX + ".log.enabled:true} && 'DB'.equals('${" + Constants.PROJECT_PREFIX + ".log.type:LOGGER}')")
     public SysLogListener sysLogListener(BaseOperationLogService logApi) {
         return new SysLogListener(data -> logApi.save(BeanPlusUtil.toBean(data, BaseOperationLogSaveVO.class)));
     }
