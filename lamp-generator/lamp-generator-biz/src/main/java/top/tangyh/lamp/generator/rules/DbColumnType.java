fe3ffa347172afc67fe96769c3b7b0bffd298a75
zuihou <244387066@qq.com>
2025-03-26 23:45:03 +0800

    feat: 代码生成支持vben5 和 soybean

--- lamp-generator/lamp-generator-biz/src/main/java/top/tangyh/lamp/generator/rules/DbColumnType.java
+++ lamp-generator/lamp-generator-biz/src/main/java/top/tangyh/lamp/generator/rules/DbColumnType.java
@@ -1,6 +1,7 @@
 package top.tangyh.lamp.generator.rules;
 
 import top.tangyh.lamp.generator.enumeration.ComponentEnum;
+import top.tangyh.lamp.generator.enumeration.SoyComponentEnum;
 import top.tangyh.lamp.generator.enumeration.VxeComponentEnum;
 
 /**
@@ -9,48 +10,48 @@ import top.tangyh.lamp.generator.enumeration.VxeComponentEnum;
  */
 public enum DbColumnType implements ColumnType {
     // 基本类型
-    BASE_BYTE("byte", null, "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    BASE_SHORT("short", null, "number", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    BASE_CHAR("char", null, "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    BASE_INT("int", null, "number", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    BASE_LONG("long", null, "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    BASE_FLOAT("float", null, "number", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    BASE_DOUBLE("double", null, "number", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    BASE_BOOLEAN("boolean", null, "boolean", ComponentEnum.PLUS_RADIO_GROUP.getValue(), VxeComponentEnum.$RADIO.getValue()),
+    BASE_BYTE("byte", null, "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.TEXT.getValue()),
+    BASE_SHORT("short", null, "number", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.TEXT.getValue()),
+    BASE_CHAR("char", null, "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.TEXT.getValue()),
+    BASE_INT("int", null, "number", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.TEXT.getValue()),
+    BASE_LONG("long", null, "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.TEXT.getValue()),
+    BASE_FLOAT("float", null, "number", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.TEXT.getValue()),
+    BASE_DOUBLE("double", null, "number", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.TEXT.getValue()),
+    BASE_BOOLEAN("boolean", null, "boolean", ComponentEnum.PLUS_RADIO_GROUP.getValue(), VxeComponentEnum.$RADIO.getValue(), SoyComponentEnum.DICT_RADIO.getValue()),
 
     // 包装类型
-    BYTE("Byte", null, "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    SHORT("Short", null, "number", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    CHARACTER("Character", null, "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    INTEGER("Integer", null, "number", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    LONG("Long", null, "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    FLOAT("Float", null, "number", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    DOUBLE("Double", null, "number", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    BOOLEAN("Boolean", null, "boolean", ComponentEnum.PLUS_RADIO_GROUP.getValue(), VxeComponentEnum.$RADIO.getValue()),
-    STRING("String", null, "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue()),
+    BYTE("Byte", null, "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.TEXT.getValue()),
+    SHORT("Short", null, "number", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.TEXT.getValue()),
+    CHARACTER("Character", null, "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.TEXT.getValue()),
+    INTEGER("Integer", null, "number", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.TEXT.getValue()),
+    LONG("Long", null, "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.TEXT.getValue()),
+    FLOAT("Float", null, "number", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.TEXT.getValue()),
+    DOUBLE("Double", null, "number", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.TEXT.getValue()),
+    BOOLEAN("Boolean", null, "boolean", ComponentEnum.PLUS_RADIO_GROUP.getValue(), VxeComponentEnum.$RADIO.getValue(), SoyComponentEnum.DICT_RADIO.getValue()),
+    STRING("String", null, "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.TEXT.getValue()),
 
     // sql 包下数据类型
-    DATE_SQL("Date", "java.sql.Date", "string", ComponentEnum.PLUS_DATE_PICKER.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    TIME("Time", "java.sql.Time", "string", ComponentEnum.PLUS_DATE_PICKER.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    TIMESTAMP("Timestamp", "java.sql.Timestamp", "string", ComponentEnum.PLUS_DATE_PICKER.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    BLOB("Blob", "java.sql.Blob", "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    CLOB("Clob", "java.sql.Clob", "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue()),
+    DATE_SQL("Date", "java.sql.Date", "string", ComponentEnum.PLUS_DATE_PICKER.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.DATETIME.getValue()),
+    TIME("Time", "java.sql.Time", "string", ComponentEnum.PLUS_DATE_PICKER.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.TIME.getValue()),
+    TIMESTAMP("Timestamp", "java.sql.Timestamp", "string", ComponentEnum.PLUS_DATE_PICKER.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.DATE.getValue()),
+    BLOB("Blob", "java.sql.Blob", "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.TEXTAREA.getValue()),
+    CLOB("Clob", "java.sql.Clob", "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.TEXTAREA.getValue()),
 
     // java8 新时间类型
-    LOCAL_DATE("LocalDate", "java.time.LocalDate", "string", ComponentEnum.PLUS_DATE_PICKER.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    LOCAL_TIME("LocalTime", "java.time.LocalTime", "string", ComponentEnum.PLUS_TIME_PICKER.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    YEAR("Year", "java.time.Year", "string", ComponentEnum.PLUS_DATE_PICKER.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    YEAR_MONTH("YearMonth", "java.time.YearMonth", "string", ComponentEnum.PLUS_MONTH_PICKER.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    LOCAL_DATE_TIME("LocalDateTime", "java.time.LocalDateTime", "string", ComponentEnum.PLUS_DATE_PICKER.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    INSTANT("Instant", "java.time.Instant", "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue()),
+    LOCAL_DATE("LocalDate", "java.time.LocalDate", "string", ComponentEnum.PLUS_DATE_PICKER.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.DATE.getValue()),
+    LOCAL_TIME("LocalTime", "java.time.LocalTime", "string", ComponentEnum.PLUS_TIME_PICKER.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.TIME.getValue()),
+    YEAR("Year", "java.time.Year", "string", ComponentEnum.PLUS_DATE_PICKER.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.DATE.getValue()),
+    YEAR_MONTH("YearMonth", "java.time.YearMonth", "string", ComponentEnum.PLUS_MONTH_PICKER.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.DATE.getValue()),
+    LOCAL_DATE_TIME("LocalDateTime", "java.time.LocalDateTime", "string", ComponentEnum.PLUS_DATE_PICKER.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.DATETIME.getValue()),
+    INSTANT("Instant", "java.time.Instant", "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.TEXT.getValue()),
 
     // 其他杂类
-    MAP("Map", "java.util.Map", "any", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    BYTE_ARRAY("byte[]", null, "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    OBJECT("Object", null, "any", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    DATE("Date", "java.util.Date", "string", ComponentEnum.PLUS_DATE_PICKER.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    BIG_INTEGER("BigInteger", "java.math.BigInteger", "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue()),
-    BIG_DECIMAL("BigDecimal", "java.math.BigDecimal", "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue());
+    MAP("Map", "java.util.Map", "any", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.TEXT.getValue()),
+    BYTE_ARRAY("byte[]", null, "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.TEXT.getValue()),
+    OBJECT("Object", null, "any", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.TEXT.getValue()),
+    DATE("Date", "java.util.Date", "string", ComponentEnum.PLUS_DATE_PICKER.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.DATETIME.getValue()),
+    BIG_INTEGER("BigInteger", "java.math.BigInteger", "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.NUMBER.getValue()),
+    BIG_DECIMAL("BigDecimal", "java.math.BigDecimal", "string", ComponentEnum.PLUS_INPUT.getValue(), VxeComponentEnum.$INPUT.getValue(), SoyComponentEnum.NUMBER.getValue());
 
     /**
      * 类型
@@ -64,13 +65,15 @@ public enum DbColumnType implements ColumnType {
     private final String tsType;
     private final String component;
     private final String vxeComponent;
+    private final String soyComponent;
 
-    DbColumnType(final String type, final String pkg, final String tsType, final String component, String vxeComponent) {
+    DbColumnType(final String type, final String pkg, final String tsType, final String component, String vxeComponent, String soyComponent) {
         this.type = type;
         this.pkg = pkg;
         this.tsType = tsType;
         this.component = component;
         this.vxeComponent = vxeComponent;
+        this.soyComponent = soyComponent;
     }
 
     @Override
@@ -97,4 +100,11 @@ public enum DbColumnType implements ColumnType {
     public String getVxeComponent() {
         return vxeComponent;
     }
+
+    @Override
+    public String getSoyComponent() {
+        return soyComponent;
+    }
+
+
 }
